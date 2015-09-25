2ebdf855ad1f6a6a9e29af7b0f3860134c8bd9d8
David Bernheisel <david@bernheisel.com>
2015-09-25 17:35:04 -0400

    Fix eBay Controller and Model

--- app/controllers/ebay_controller.rb
+++ app/controllers/ebay_controller.rb
@@ -1,107 +1,26 @@
-require 'open-uri'
+class EbayController < APIController
 
-class EBayController < APIController
+  def show
+    result = Ebay.new.cheapest_result(keyword: params[:keyword])
 
-  @dev_id = ENV['EBAY_DEVID'] #same between sandbox/prod
-  @app_id_sandbox = ENV['EBAY_APPID_SANDBOX']
-  @cert_id_sandbox = ENV['EBAY_CERTID_SANDBOX']
-  @app_id = ENV['EBAY_APPID']
-  @cert_id = ENV['EBAY_CERTID']
-
-  attr_reader :cheapest_result
-
-  def initialize
-    reference = get_ebay_reference_id(keyword: "Nintendo Wii U")
-    results = get_results(ebay_reference_id: reference)
-    @cheapest_result = get_cheapest_result(results)
-  end
-
-  def get_ip_zip(zip: nil)
-    zip = open('http://whatismyip.akamai.com').read unless zip
-    url = "http://geocoder.ca/
-            #{zip}
-            ?json=1"
-    JSON.parse(open(url).read)["usa"]["zip"] || ""
-  end
-
-  def get_cheapest_result(results)
-    results.sort_by { |m| m["total_price"] }.first
-  end
-
-  def get_results(ebay_reference_id:, sandbox: false)
-    sandbox ? app_id = @app_id_sandbox : app_id = @app_id
-    url = "http://svcs.ebay.com/services/search/FindingService/v1
-            ?OPERATION-NAME=findItemsByProduct
-            &SERVICE-VERSION=1.0.0
-            &GLOBAL-ID=EBAY-US
-            &SECURITY-APPNAME=
-            &RESPONSE-DATA-FORMAT=JSON
-            &REST-PAYLOAD=
-            &buyerPostalCode=#{get_ip_zip}
-            &itemFilter(0).name=Condition
-            &itemFilter(0).value(0)=1000
-            &itemFilter(0).value(1)=1500
-            &itemFilter(0).value(2)=2000
-            &itemFilter(1).name=Currency
-            &itemFilter(1).value(0)=USD
-            &itemFilter(2).name=ListingType
-            &itemFilter(2).value(0)=AuctionWithBIN
-            &itemFilter(2).value(1)=FixedPrice
-            &itemFilter(3).name=LocatedIn
-            &itemFilter(3).value(0)=US
-            &productId.@type=ReferenceID
-            &productId=#{ebay_reference_id}
-            &paginationInput.entriesPerPage=10
-            &sortOrder=PricePlusShippingLowest"
-    response = JSON.parse(open(URI.escape(url)).read)
-    matches = []
-    response["findItemsByProductResponse"]["searchResult"]["item"].each do |p|
-      shipping_cost = p["shippingInfo"][0]["shippingServiceCost"][0]["__value__"].to_f
-      buy_it_now_price = p["listingInfo"][0]["convertedBuyItNowPrice"][0]["__value__"].to_f
-
-      matches << {
-        item_id: p["itemId"],
-        category_name: p["primaryCategory"][0]["categoryName"],
-        shipping_cost: shipping_cost,
-        buy_it_now_price: buy_it_now_price,
-        total_price: shipping_cost + buy_it_now_price,
-        condition: p["condition"][0]["conditionDisplayName"][0],
-        url: "http://www.ebay.com/itm/#{p['itemId']}"
-      }
-    matches
+    begin
+      respond_to do |format|
+        format.json { render json: result.to_json, status: 200 }
+        format.xml  { render xml: result.to_xml, status: 200 }
+      end
+    rescue
+      render json: { Error: "Something went wrong" }
     end
-
-
   end
 
-  def get_ebay_reference_id(keyword:, sandbox: false)
-    sandbox ? app_id = @app_id_sandbox : app_id = @app_id
-    url = "http://open.api.ebay.com/shopping?
-             callname=FindProducts&
-             responseencoding=JSON&
-             appid=#{app_id}&
-             siteid=0&
-             version=525&
-             QueryKeywords=#{URI.escape(keyword)}&
-             AvailableItemsOnly=true&
-             MaxEntries=10"
-    response = JSON.parse(open(URI.escape(url)).read)
-    potential_title_matches = []
-    response["Product"].each { |p| potential_title_matches << p["Title"] }
-    product_index = potential_title_matches.each { |p| p.starts_with?(keyword) break p.index }
-    product_id_index = response["Product"][product_index].each { |pid| pid["Type"] == "Reference" break pid.index }
-    response["Product"][product_index]["ProductID"][product_id_index]["Value"]
+  def show_results
+    params[:number] ||= 10
+    results = Ebay.new.search(keyword: params[:keyword], entries: params[:number])
+    respond_to do |format|
+      format.json { render json: results.to_json, status: 200 }
+      format.xml  { render xml: results.to_xml, status: 200 }
+    end
   end
 
-
-# http://svcs.ebay.com/services/search/FindingService/v1?
-#    OPERATION-NAME=findItemsByProduct&
-#    SERVICE-VERSION=1.0.0&
-#    SECURITY-APPNAME=@app_id_sandbox&
-#    RESPONSE-DATA-FORMAT=JSON&
-#    REST-PAYLOAD&
-#    paginationInput.entriesPerPage=10&
-#    productId.@type=UPC&
-#    productId=53039031
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get   'ebay/result/:keyword' => 'ebay#show'
+  get   'ebay/results/:keyword' => 'ebay#show_results'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
