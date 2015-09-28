5fecb059d5250cb9f081350a0b461db26ead083c
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-09-27 21:41:55 -0400

    Fix Amazon model tests

--- test/models/amazon_test.rb
+++ test/models/amazon_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 require './app/models/amazon.rb'
+require 'byebug'
 class Amazon
-  def get_az_list
+  def get_az_list(a, b)
     file = File.read(Rails.root.join('app', 'assets', 'json', 'most_gifted_toys.json'))
     @response = JSON.parse(file)
   end
@@ -14,13 +15,13 @@ class AmazonTest < ActionController::TestCase
 
   test "can get AZ most x list" do
     request = Amazon.new
-    request.get_az_list
+    request.get_az_list(1, 2)
     assert request.response.to_h.to_s.include?("Cards Against Humanity")
   end
 
   test "can get ASINS" do
     request = Amazon.new
-    request.get_az_list
+    request.get_az_list(1, 2)
     asin_list = request.extract_asins
 
     assert asin_list.include?("B003WFKOSS")
@@ -28,8 +29,7 @@ class AmazonTest < ActionController::TestCase
 
   test "can get UPC and price list" do
     request = Amazon.new
-    response = request.make_a_request
-
-    assert response.include?("VTech Smart")
+    response = request.make_a_request("2625373011", "NewReleases")
+    assert response[0].title.include?("Cards Against Humanity")
   end
 end
